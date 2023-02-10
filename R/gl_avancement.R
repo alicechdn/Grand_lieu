2ad05149b091541e9490afc87e28a38b042a343a
alicechdn <124247674+alicechdn@users.noreply.github.com>
2023-02-10 17:01:48 +0100

    Update gl_avancement.R

--- R/gl_avancement.R
+++ R/gl_avancement.R
@@ -1,4 +1,4 @@
-###################### PARTIE 1 - CREATION DU JDD ############
+###################### PARTIE 1 - CREATION DU JDD -----------------------------
 ############## 1 - CHARGEMENT DU JDD BRUT ########
 
 
@@ -16,7 +16,9 @@ dim(pod)# dimensions : nbre de lignes puis de colonnes
 ############## 2 - CREATION DU JDD EXPLOITABLE SUR R - une ligne par obs ##############
 
 
-#######
+####### a - Nettoyer lignes et colonnes et renommer variables #####
+
+
 pod<-subset(pod, Site != "TOTAL")#subset pour tout garder sauf les lignes total (avec !=)
 pod <- pod[,-c(3:4)]#enlever lat et long
 pod[is.na(pod)] <- 0#remplacer les na par des 0 
@@ -24,22 +26,29 @@ colnames(pod)[1:2] <- c("ANNEE","SITE")#renommer les colonnes
 dim(pod)#dimensions : on a 20 lignes en moins et deux colonnes en moins? alors c est ok
 View(pod)
 
-#######
+
+####### b - Basculer le jdd en 1 ligne = 1 observation #####
+
+
 #Fonction reshape pour restructurer le jdd
 #melt permet de passer des colonnes en lignes, en creant une nouvelle variable a partir de ces lignes 
 PE <- reshape2::melt(pod,id=c("ANNEE","SITE"),value.name = "ABONDANCE")
 colnames(PE)[3] <- "ESPECE"
 
 
-
-############## 3 - CREATION DES JDD DE VARIABLES EXPLICATIVES ########
-
-
-
+############## 3 - CREATION DES JDD DE VARIABLES EXPLICATIVES -----------------
 ####### a - Les points GPS des points d ecoute : pod_site #####
+
+
 pod_site <- unique(pod[,3:4])
 
-####### b - Les noms vernaculaires des oiseaux : code_crbpo #####
+####### b - Caractéristique de l'habitat : ######
+
+#### en attente des donnees 
+
+####### c - Les noms vernaculaires des oiseaux : code_crbpo #####
+
+
 #library(readxl) # au cas ou il faut la relancer
 code_crbpo <- read_excel("DATA/noms_vernaculaires.xlsx", col_names = FALSE)#chargement du jdd 
 titre<-c("ESPECE", "NOM_FR_BIRD")#nom des variables dans la matrice
@@ -54,7 +63,11 @@ library(dplyr)
 PE <-select(PE, ANNEE, SITE, ESPECE, NOM_FR_BIRD,ABONDANCE)#data puis ordre des colonnes
 View(PE)
 
-####### c - La famille et l ordre de chaque espece : info_esp #####
+
+####### d - La famille et l ordre de chaque espece : info_esp #####
+
+
+
 library(readr)
 info_esp <- read_csv("DATA/espece.csv")
 View(info_esp)
@@ -70,7 +83,13 @@ View(PE_info)
 rea <- unique(subset(PE_info, is.na(family_tax), select = "ESPECE"))#recherche des mauvais code espece 
 rea # si egal à 0 alors c est ok 
 
-####### d - Le poids des esp, leur regime alimentaire et autre #####
+
+
+
+####### e - Le poids des esp, leur regime alimentaire et autre : geb #####
+
+
+
 #chargement du jdd avec les poids moyen des esp 
 #attention triche : je l ai converti en csv 
 library(readr)
@@ -85,5 +104,17 @@ View(geb)
 #geb$POIDS <- as.numeric(geb$POIDS) #ne pas faire pour l'instant car introduit des NA
 
 PE <- merge(PEc,geb, all.x = TRUE, by.x = "ESPECE", by.y = "CODE")#fusion des deux jdd 
-####### e - Gradient de specialisation : 
-#######
+
+
+
+
+####### f - Gradient de specialisation : ind_fonction  #######
+
+
+library(readr)
+ind_fonction<- read_csv("DATA/espece_indicateur_fonctionel.csv")
+View(ind_fonction)
+ind_fonction$pk_species<- ifelse(ind_fonction$pk_species == "LANSEN" , "LANSER" , ind_fonction$pk_species)
+#j ai remplace directement dans la variable pk_species 
+
+
